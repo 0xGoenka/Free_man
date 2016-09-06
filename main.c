9fb48772e5e4dc7aa02fb897cfa62d8f2abe7d9f
eleclet <eleclet@student.42.fr>
2016-09-06 18:24:39 +0200

    explain in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eleclet <eleclet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/01 16:10:22 by eleclet           #+#    #+#             */
-/*   Updated: 2016/09/06 18:15:29 by eleclet          ###   ########.fr       */
+/*   Updated: 2016/09/06 18:24:12 by eleclet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int main()
 	char *s;
 	void *p;
 
+	s = gc_alloc(10000); // use it as malloc
 	s = gc_alloc(10000);
 	s = gc_alloc(10000);
-	s = gc_alloc(10000);
-	p = gc_add(malloc(10));
-	gc_free();
+	p = gc_add(malloc(10)); //allow you to add function that malloc for u such as strdup strnew etc ..
+	gc_free(); // put this at the end to free all malloc
 	return (0);
 }
 
